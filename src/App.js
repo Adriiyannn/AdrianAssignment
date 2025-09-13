a8a2d3b0e693d442475fb89c5a4dfc6279bf4d1d
Adriiyannn <adriancaballero.202300757@gmail.com>
2025-09-14 03:41:45 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Content from "./Content";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Sidebar />
+        <Content />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Content.js
@@ -0,0 +1,11 @@
+Function Content() {
+  return (
+    <main className="content">
+      <h2>Welcome!</h2>
+      <p>This is the main content area of my React app.</p>
+      <img src="https://via.placeholder.com/300" alt="Sample" />
+    </main>
+  );
+}
+
+export default Content;
